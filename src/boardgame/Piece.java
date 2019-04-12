6021e2cf951ef5287a23f3183e964bcde49a43c0
Carlos Augusto Eberhardt <krlos.eberhardt@gmail.com>
2019-04-12 16:07:24 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Piece {
+
+	protected Position position;
+	private Board boad;
+	
+	public Piece(Board boad) {
+		this.boad = boad;
+		position = null;
+	}
+
+	protected Board getBoad() {
+		return boad;
+	}	
+	
+}
